6ce319d34fecc1454bd2af6af268ea595fe32a02
Facundo Prieto <facundo.prieto321@gmail.com>
2021-08-26 02:33:14 -0300

    fix cover type missing

--- types.d.ts
+++ types.d.ts
@@ -1,24 +1,10 @@
 type project = {
     id: number;
     title: string;
-    logo: string;
-    link: string;
     desc: string;
-    blurHash: string;
+    cover:string
+    url: string;
     technologies: string[];
     tech: string[];
   };
-
-  type article = {
-    title: string;
-    desc: string;
-    link: string;
-    views: string;
-    external: boolean;
-    isNew: boolean;
-    readTime: string;
-    published: string;
-    tags: string[];
-    github_url: string;
-  }
-  
+          
